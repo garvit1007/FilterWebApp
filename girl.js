8bf06b10fce305bb65f8c17fce35bba42c61cdd3
garvit1007 <97176509+garvit1007@users.noreply.github.com>
2023-05-26 17:40:07 +0530

    Add files via upload

---
+++ girl.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot_girl(){
+    save('myFilterImage.png');
+}
